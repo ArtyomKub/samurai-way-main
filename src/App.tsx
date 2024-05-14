2a4c6a22060a0cfe74a2e796d315c79fee847861
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-14 12:31:03 +0200

    Create const Header and Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,31 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    )
+}
 
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Messages</a>
+        </div>
+    )
+}
+const Technologies = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
         </div>
     )
 }
