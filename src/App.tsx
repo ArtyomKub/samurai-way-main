5365b3f05c63ae75b229dbee4dcde54427131810
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-14 16:34:09 +0200

    import component Technologies.tsx in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Technologies} from './Technologies';
+import {Header} from './Header';
+
 
 const App = () => {
     return (
         <div>
             <Header/>
             <Technologies/>
-        </div>
-    )
-}
-
-const Header = () => {
-    return (
-        <div>
-            <a href="">Home</a>
-            <a href="">News Feed</a>
-            <a href="">Messages</a>
-            <a href="">Messages</a>
-        </div>
-    )
-}
-const Technologies = () => {
-    return (
-        <div className="App">
-            <ul>
-                <li>CSS</li>
-                <li>HTML</li>
-                <li>JS</li>
-                <li>React</li>
-            </ul>
         </div>
     )
 }
